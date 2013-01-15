ae2e489d43c8dc73c5144598ac8aca200ddfa85a
naveedmurtuza@gmail.com <naveedmurtuza@gmail.com@ddf8a125-ff2d-eeb0-573c-849560fc6d8b>
2013-01-15 17:22:23 +0000

    added comments

--- trunk/wia4j/src/com/wia/api/Wia4jTest.java
+++ trunk/wia4j/src/com/wia/api/Wia4jTest.java
@@ -19,11 +19,20 @@ public abstract class Wia4jTest {
     public static void main(String[] args) {
         Wia4j wia4j = new Wia4j();
         Map<Integer, Integer> props = new HashMap<>();
+        //scan B&W
         props.put(Wia4j.WIA_IPS_CUR_INTENT, WiaImageIntent.TextIntent.comEnumValue());
         try {
+            //scan using the default dialogs from WIA
             wia4j.scan("D:\\images\\test1.png");
+            /**
+             * Scans a single page from the feeder Warning! if there are
+             * multiple pages in the feeder, the next page will be stucked in
+             * the feeder!
+             */
             wia4j.scan("D:\\images\\test2.png", true, true, WiaImageFormat.wiaFormatPNG, props);
+            //scans page from flatbed
             wia4j.scan("D:\\images\\test3.png", false, true, WiaImageFormat.wiaFormatPNG, props);
+            // change intent to color
             props.remove(Wia4j.WIA_IPS_CUR_INTENT);
             props.put(Wia4j.WIA_IPS_CUR_INTENT, WiaImageIntent.ColorIntent.comEnumValue());
             wia4j.scan("D:\\images\\test4.png", true, true, WiaImageFormat.wiaFormatPNG, props);
@@ -33,6 +42,7 @@ public abstract class Wia4jTest {
             Logger.getLogger(Wia4jTest.class.getName()).log(Level.SEVERE, null, ex);
         }
 
+        //scan all pages in the feeder
         try {
             wia4j.scan("D:\\images", true, WiaImageFormat.wiaFormatJPEG, props, new ImageCallback<String>() {
                 @Override
